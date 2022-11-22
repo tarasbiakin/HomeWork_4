dcfd9196bd857392d3032d827c72ee38d406920f
tarasbiakin <115660593+tarasbiakin@users.noreply.github.com>
2022-11-22 19:57:36 +0300

    27

--- 27/Program.cs
+++ 27/Program.cs
@@ -4,14 +4,15 @@ Console.Write("Введите число:");
 int number = Convert.ToInt32(Console.ReadLine());
 
 
-int Digits(int num)
+int Sum(int num)
 {
     int result = 0;
-    if (num > 0)
-    {
-     while (num > 0){ result = result+num % 10;num=num/10;}
-    }
-    else{Console.Write("число меньше '0' ");}
+    if (num > 0){
+    while (num > 0){ result = result+num % 10;num=num/10;
+    }System.Console.WriteLine("Сумма чисел:"+result);}
+    else{
+        Console.Write("число меньше '0' ");}
     return result;
 }
-Console.WriteLine("Сумма чисел: "+Digits(number));
+
+Sum(number);
